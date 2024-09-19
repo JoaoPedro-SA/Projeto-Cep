acb30895a8600bac96b423ac9d4fdce11243cadf
JoaopedroSA-cmd <antunesjoaopedro3@gmail.com>
2024-09-19 09:38:16 -0300

    v2

--- java224.js
+++ java224.js
@@ -1,84 +1,88 @@
-async function pegaCEP(){
-     let cep = document.querySelector(".inputCEP");  
+async function pegaCEP() {
+     let cep = document.querySelector(".inputCEP");
      return cep.value
 }
 
-function valor_nulo (valor) {
+function valor_nulo(valor) {
      if (valor == null || valor == "" || valor == undefined) {
-          return `Sem registro`
+          return `Não temos essa informação`
      }
      return valor
 }
-     
+
 async function ConsultaCEP(cep) {
      const url = `https://viacep.com.br/ws/${cep}/json/`;
-     try{
+     try {
           const response = await fetch(url);
-          if (!response.ok){
+          if (!response.ok) {
                throw new Error(response.statusText);
-         }
+          }
           const json = await response.json();
           console.log(json);
-          let jsonObject = json;
-          return jsonObject
-}
-     catch(error){
-     console.log(error.message);
-     return []
-}
+          return json // retona os dados da api 
+     }
+     catch (error) {
+          console.log(error.message);
+          return []
+     }
 
 }
 
-async function Para_execulcao(valor_cel){
+async function para_execulcao(valor_cep) {
      let parada = false;
-     for (let i = 0; i < valor_cel.length; i++){
-          if (!valor_cel || valor_cel.length !== 8) {
+     for (let i = 0; i < valor_cep.length; i++) {
+          if (!valor_cep || valor_cep.length !== 8) {
                parada = true; // CEP inválido
-             }// CEP válido
-           }
-           
-
+          }// CEP válido
+     }
      return parada;
+}
+
+main = () => {
+
+     const button = document.querySelector(".button");
+
+     button.addEventListener('click', consulta1 = async () => {
+          let cep = null;
+          let texto = document.querySelector(".receber2");
+
+          cep = await pegaCEP();
+          let erro = document.querySelector('.erro_cep');
+          let condicao = await para_execulcao(cep);
+          if (condicao) {
+               erro.textContent = "CEP INVALIDO: SO PODE NUMERO ATÉ 8 DIGITOS";
+               erro.style.display = 'block';
+               // throw new Error("Erro!");
+               return;
+          }
+          else {
+               erro.style.display = 'none';
+          }
+
+          const resultado = await ConsultaCEP(cep);
+          texto.innerHTML = `
+     <div><b> CEP:</b>  ${valor_nulo(resultado.cep)}</div>
+     <div><b> Logradouro:</b> ${valor_nulo(resultado.logradouro)}</div>
+     <div><b> Bairro:</b> ${valor_nulo(resultado.bairro)}</div>
+     <div><b> Cidade:</b> ${valor_nulo(resultado.localidade)}</div)>
+     <div><b> Estado:</b> ${valor_nulo(resultado.uf)}</div>
+     <div><b> unidade:</b> ${valor_nulo(resultado.unidade)}</div>
+     <div><b> localidade:</b> ${valor_nulo(resultado.localidade)}</div>
+     <div><b> regiao:</b> ${valor_nulo(resultado.regiao)}</div>
+     <div><b> ibge:</b> ${valor_nulo(resultado.ibge)}</div>
+     <div><b> gia:</b> ${valor_nulo(resultado.gia)}</div>
+     <div><b> ddd:</b> ${valor_nulo(resultado.ddd)}</div>
+     <div><b> siafi:</b> ${valor_nulo(resultado.siafi)}</div>
+`;
+     })
+
+     let inputCEP = document.querySelector(".inputCEP");
+     inputCEP.addEventListener('keydown', async (e) => {
+          if (e.key === 'Enter') {
+               consulta1();
+          }
+     })
 
 }
 
-
-
-const button = document.querySelector(".button");
-let cep = null;
-let texto = document.querySelector(".receber2");
-button.addEventListener('click', consulta1 = async () => { 
-     cep = await pegaCEP();
-     let erro = document.querySelector('.erro_cep');
-     let condicao = await Para_execulcao(cep);
-     if (condicao === true){
-          erro.textContent = "CEP INVALIDO: SO PODE NUMERO ATÉ 8 DIGITOS";
-          erro.style.display = 'block';
-          throw new Error("Erro!");
-     }
-     else{
-          erro.style.display = 'none';
-     }
-
-     const resultado = await ConsultaCEP(cep);
-     texto.innerHTML = `
-     <div><b> CEP:</b>  ${valor_nulo (resultado.cep)}</div>
-     <div><b> Logradouro:</b> ${valor_nulo (resultado.logradouro)}</div>
-     <div><b> Bairro:</b> ${valor_nulo (resultado.bairro)}</div>
-     <div><b> Cidade:</b> ${valor_nulo (resultado.localidade)}</div)>
-     <div><b> Estado:</b> ${valor_nulo (resultado.uf)}</div>
-     <div><b> unidade:</b> ${valor_nulo (resultado.unidade)}</div>
-     <div><b> localidade:</b> ${valor_nulo (resultado.localidade)}</div>
-     <div><b> regiao:</b> ${valor_nulo (resultado.regiao)}</div>
-     <div><b> ibge:</b> ${valor_nulo (resultado.ibge)}</div>
-     <div><b> gia:</b> ${valor_nulo (resultado.gia)}</div>
-     <div><b> ddd:</b> ${valor_nulo (resultado.ddd)}</div>
-     <div><b> siafi:</b> ${valor_nulo (resultado.siafi)}</div>
-`;
-})
-let inputCEP = document.querySelector(".inputCEP");
-inputCEP.addEventListener('keydown', async (e) => {
-     if (e.key === 'Enter') {
-          consulta1();
-     }
-})
+main();
